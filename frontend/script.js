3df0d43b65efaa2109c9c0d949d0eab64327cdc5
windiewhale <feketebodza@gmail.com>
2022-04-06 18:56:05 +0200

    add css

--- frontend/script.js
+++ frontend/script.js
@@ -3,17 +3,17 @@ function loadEvent() {
     let rootElement = document.getElementById("root")
     
     fetch("beer.json")
-            .then(response => response.json())
-            .then(data => {
-                data.cards.forEach(card => {
-                    let htmlC = `
-                    <div class="card">
-                        <h2>${card.title}</h2>
-                        <h4>${card.sub}</h4>
-                        <p>${card.text}</p>
-                    </div>`;
-                    rootElement.insertAdjacentHTML("beforeend", htmlC)
-                });
-            })  
+        .then(response => response.json())
+        .then(data => {
+            data.cards.forEach(card => {
+                let htmlC = `
+                <div class="card">
+                    <h2>${card.title}</h2>
+                    <h4>${card.sub}</h4>
+                    <p>${card.text}</p>
+                </div>`;
+                rootElement.insertAdjacentHTML("beforeend", htmlC)
+            });
+        })  
 }
 window.addEventListener("load", loadEvent);
